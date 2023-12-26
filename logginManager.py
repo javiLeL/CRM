0a714bd8be08713f6115319a62525b92758d7795
Javi.LeL <javier212002@gmail.com>
2023-12-26 19:33:34 +0100

    funcion insertat

--- logginManager.py
+++ logginManager.py
@@ -1,28 +1,49 @@
 import sqlite3
 
 class log:
-    def __init__(self, empresa, nombre, password):
-        self.empresa=empresa
-        self.nombre=nombre
-        self.password=password
+    def __init__(self, empresa, correo_electronico, password):
+        self.empresa=str(empresa)
+        self.correo_electrónico=str(correo_electronico)
+        self.password=str(password)
         try:
-            miConexion = sqlite3.connect("LOGIN")
+            miConexion = sqlite3.connect("LOGGIN")
             miCursor = miConexion.cursor()
             miCursor.execute("""
-                                CREATE TABLE LOGIN (
-                                    nombre_empresa VARCHAR(50) NOT NULL,
-                                    correo_electrónico VARCHAR(50) NOT NULL PRIMARY KEY,
-                                    contrasena VARCHAR(50) NOT NULL
+                                CREATE TABLE LOGGIN (
+                                    correo_electronico VARCHAR(50) PRIMARY KEY,
+                                    nombre_empresa VARCHAR(50),
+                                    password VARCHAR(50)
                             );""")
             miConexion.commit()
             miConexion.close()
         except:
             print("Ya esta creada la base de datos LOGGIN")
-    def register():
-        pass
+        print("Creada conexion")
+    def register(self):
+            try:
+                miConexion = sqlite3.connect("LOGGIN")
+                miCursor = miConexion.cursor()
+                
+                miCursor.executemany("INSERT INTO LOGGIN VALUES (?, ?, ?)", [(str(self.empresa), str(self.correo_electrónico), str(self.password))])
+
+                miConexion.commit()
+                miConexion.close()
+            except:
+                print("error")
 
     def loggin ():
         pass
 
-    def isNewLoggin():
-        pass
+    def isNewLoggin(self):
+        try:
+            miConexion = sqlite3.connect("LOGGIN")
+            miCursor = miConexion.cursor()
+
+            miCursor.execute("SELECT * FROM LOGGIN WHERE correo_electronico=?", [(self.correo_electronico)])
+            lista = miCursor.fetchall()
+            print(lista)
+        except:
+            self.register()
+
+    def __str__(self):
+        return "Empresa: "+self.empresa+" Correo: "+self.correo_electrónico+" passwd: "+self.password
--- loginWindow.py
+++ loginWindow.py
@@ -1,10 +1,14 @@
 from tkinter import *
-from tkinter import messagebox
 import logginManager
 
 root = Tk()
 miFrame = Frame(root, width=700, height=500)
 miFrame.pack()
+
+def loggin(empresa, correo_electronico, password):
+    log = logginManager.log(empresa, correo_electronico, password)
+    print(log)
+    log.register()
 
 root.title("Loggin CRM")
 
@@ -17,10 +21,10 @@ empresa2 = Label(miFrame, text = "Empresa: ").grid(row = row, column = 0, sticky
 
 row+=1
 
-nombre = StringVar()
-nombre2 = Entry(miFrame, textvariable = nombre)
-nombre2.grid(row = row, column = 1, padx=10, pady=10)
-nombre2 = Label(miFrame, text = "Nombre: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+correo_electronico = StringVar()
+correo_electronico2 = Entry(miFrame, textvariable = correo_electronico)
+correo_electronico2.grid(row = row, column = 1, padx=10, pady=10)
+correo_electronico2 = Label(miFrame, text = "Correo Electronico: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
 
 row+=1
 
@@ -41,7 +45,8 @@ row+=1
 botonCancelar = Button (miFrame, text="Cancelar")
 botonCancelar.grid(row = row, column = 0, pady=20)
 
-botonEnvio = Button (miFrame, text="Enviar")
+# botonEnvio = Button (miFrame, text="Enviar", command=lambda:loggin(empresa.get(), correo_electronico.get(), password.get()))
+botonEnvio = Button (miFrame, text="Enviar", command=lambda:loggin(empresa=empresa.get(), correo_electronico=correo_electronico.get(), password=password.get()))
 botonEnvio.grid(row = row, column = 1)
 
 root.mainloop()
