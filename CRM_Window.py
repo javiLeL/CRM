2f8f8f90bbd1d608233e23754d6852c1e08ab5a2
Javi.LeL <javier212002@gmail.com>
2023-12-30 12:29:00 +0100

    añadiendo un menu al CRM

--- CRM_Window.py
+++ CRM_Window.py
@@ -6,12 +6,38 @@ def CRM_Window():
     miFrame = Frame(CRM_Window_VAR, width=700, height=500)
     miFrame.pack()
     CRM_Window_VAR.title("CRM")
-    
-    row = 0
-    
-    empresa = StringVar()
-    empresa2 = Entry(miFrame, textvariable = empresa)
-    empresa2.grid(row = row, column = 1, padx=10, pady=10)
-    empresa2 = Label(miFrame, text = "Empresa: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    barraMenu = Menu(CRM_Window_VAR)
+    CRM_Window_VAR.config(menu=barraMenu, width="300", height="200")
+
+    contactosMenu = Menu(barraMenu, tearoff=0)
+    contactosMenu.add_command(label = "Nuevo")
+    contactosMenu.add_command(label = "Modificar")
+    contactosMenu.add_command(label = "Leer")
+    contactosMenu.add_command(label = "Borrar")
+
+    oportunidadMenu = Menu(barraMenu, tearoff=0)
+    oportunidadMenu.add_command(label = "Nueva")
+    oportunidadMenu.add_command(label = "Modificar")
+    oportunidadMenu.add_command(label = "Borrar")
+
+    presupuestosMenu = Menu(barraMenu, tearoff=0)
+    presupuestosMenu.add_command(label = "Nuevo")
+    presupuestosMenu.add_command(label = "Modificar")
+    presupuestosMenu.add_command(label = "Leer")
+    presupuestosMenu.add_command(label = "Borrar")
+
+    productosMenu = Menu(barraMenu, tearoff=0)
+    productosMenu.add_command(label = "Nuevo")
+    productosMenu.add_command(label = "Modificar")
+    productosMenu.add_command(label = "Leer")
+    productosMenu.add_command(label = "Borrar")
+
+    barraMenu.add_cascade(label = "Contactos", menu = contactosMenu)
+    barraMenu.add_cascade(label = "Contactos", menu = oportunidadMenu)
+    barraMenu.add_cascade(label = "Presupuestos", menu = presupuestosMenu)
+    barraMenu.add_cascade(label = "Productos", menu = productosMenu)
 
     CRM_Window_VAR.mainloop()
+
+CRM_Window()
