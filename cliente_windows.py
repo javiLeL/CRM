478b22c49baf452230b73a88cf83ee6f529fc684
Javi.LeL <javier212002@gmail.com>
2024-01-18 17:53:38 +0100

    añadiendo la ventana para ver los clientes

--- cliente_windows.py
+++ cliente_windows.py
@@ -104,7 +104,6 @@ def ventana_añadir(bbdd):
     iva2.grid(row = row, column = 1, padx=10, pady=10)
     iva2 = Label(miFrame, text = "IVA: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
 
-
     row += 1
 
     botonEnvio = Button (miFrame, text="Cancelar", command=exit_btn)
@@ -114,14 +113,86 @@ def ventana_añadir(bbdd):
     botonEnvio.grid(row = row, column = 1)
 
 def ventana_ver(bbdd):
-    global ventana_añadir_var
+    global ventana_ver_var
     global empresa
     empresa = bbdd
-    ventana_añadir_var = Toplevel()
-    miFrame = Frame(ventana_añadir_var, width=700, height=500)
+    ventana_ver_var = Toplevel()
+    miFrame = Frame(ventana_ver_var, width=700, height=500)
     miFrame.pack()
-    ventana_añadir_var.title("Ver Clientes")
+    ventana_ver_var.title("Ver Clientes")
     
+    row = 0
+
     listaContactos = select_clientes()
     comboNuevas = ttk.Combobox(miFrame, values = listaContactos, state = "readonly")
-    comboNuevas.grid(row = 1, column = 0, columnspan=2, sticky="e", padx=10, pady=10)
+    comboNuevas.grid(row = row, column = 1, columnspan=2, sticky="e", padx=10, pady=10)
+    comboNuevas = Label(miFrame, text = "ID: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    nombre = StringVar()
+    nombre2 = Entry(miFrame, textvariable = nombre)
+    nombre2.grid(row = row, column = 1, padx=10, pady=10)
+    nombre2 = Label(miFrame, text = "Nombre: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    calle = StringVar()
+    calle2 = Entry(miFrame, textvariable = calle)
+    calle2.grid(row = row, column = 1, padx=10, pady=10)
+    calle2 = Label(miFrame, text = "Calle: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    codigo_postal = StringVar()
+    codigo_postal2 = Entry(miFrame, textvariable = codigo_postal)
+    codigo_postal2.grid(row = row, column = 1, padx=10, pady=10)
+    codigo_postal2 = Label(miFrame, text = "Codigo Postal: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    ciudad = StringVar()
+    ciudad2 = Entry(miFrame, textvariable = ciudad)
+    ciudad2.grid(row = row, column = 1, padx=10, pady=10)
+    ciudad2 = Label(miFrame, text = "Ciudad: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    pais = StringVar()
+    pais2 = Entry(miFrame, textvariable = pais)
+    pais2.grid(row = row, column = 1, padx=10, pady=10)
+    pais2 = Label(miFrame, text = "Pais: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    telefono = StringVar()
+    telefono2 = Entry(miFrame, textvariable = telefono)
+    telefono2.grid(row = row, column = 1, padx=10, pady=10)
+    telefono2 = Label(miFrame, text = "Telefono: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    persona_contacto = StringVar()
+    persona_contacto2 = Entry(miFrame, textvariable = persona_contacto)
+    persona_contacto2.grid(row = row, column = 1, padx=10, pady=10)
+    persona_contacto2 = Label(miFrame, text = "Persona de Contacto: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    correo_electronico = StringVar()
+    correo_electronico2 = Entry(miFrame, textvariable = correo_electronico)
+    correo_electronico2.grid(row = row, column = 1, padx=10, pady=10)
+    correo_electronico2 = Label(miFrame, text = "Correo Electronico: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    iva = StringVar()
+    iva.set("21")
+    iva2 = Entry(miFrame, textvariable = iva)
+    iva2.grid(row = row, column = 1, padx=10, pady=10)
+    iva2 = Label(miFrame, text = "IVA: ").grid(row = row, column = 0, sticky="e", padx=10, pady=10)
+
+    row += 1
+
+    botonEnvio = Button (miFrame, text="Cancelar", command=exit_btn)
+    botonEnvio.grid(row = row, column = 0)
